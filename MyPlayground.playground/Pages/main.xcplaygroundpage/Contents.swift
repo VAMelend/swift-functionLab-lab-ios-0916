7274cc911b03060e357dadaa10828043921a54cf
Victoria Melendez <Victoria@Victorias-MacBook-Pro.local>
2016-08-30 10:04:04 -0400

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,6 +12,12 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWorld() {
+    let greeting = "Hello, world!"
+    print(greeting)
+}
+
+helloWorld()
 
 
 
@@ -23,19 +29,29 @@
 
 
 
+func declareC() {
+    let jars = "jelly"
+    print (jars)
+}
+
+declareC()
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
-
-
+func newPerson(name: String) {
+    print ("How are you, \(name)?")
+}
+newPerson("Justin")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
+var lars = "Donnie"
+newPerson(lars)
 
 
 
@@ -45,7 +61,12 @@
  */
 // write your code here
 
-
+func sayHello() {
+    let personName = "Jen"
+    let greeting = "Hello, " + personName + "!"
+    print (greeting)
+}
+sayHello()
 
 
 /*:
